1f1f1634a31453587d54ef48f16243ec82b79298
Jeffrey <you@example.com>
2019-07-22 22:03:40 +0300

    renamed wild_images to images

--- js/scrips.js
+++ js/scrips.js
@@ -1,16 +1,24 @@
-$(document).ready(function () {
-  var slideIndex = 0;
-  carousel();
+$(document).ready(function() {
+    var slideIndex = 0;
+    carousel();
 
-  function carousel() {
-    var i;
-    var x = document.getElementsByClassName("mySlides");
-    for (i = 0; i < x.length; i++) {
-      x[i].style.display = "none";
-    }
-    slideIndex++;
-    if (slideIndex > x.length) { slideIndex = 1 }
-    x[slideIndex - 1].style.display = "block";
-    setTimeout(carousel, 2000); // Change image every 2 seconds
-  }
+    function carousel() {
+        var i;
+        var x = document.getElementsByClassName("mySlides");
+        for (i = 0; i < x.length; i++) {
+            x[i].style.display = "none";
+        }
+        slideIndex++;
+        if (slideIndex > x.length) { slideIndex = 1 }
+        x[slideIndex - 1].style.display = "block";
+        setTimeout(carousel, 3000); // Change image every 2 seconds
+		}
+		// $("mySlides").hover(function () {
+		// 		// over
+		// 		document.getElementById('popup').style.display = 'block';				
+		// 	}, function () {
+		// 		// out
+		// 		document.getElementById('popup').style.display = 'none';
+		// 	}
+		// );
 });
